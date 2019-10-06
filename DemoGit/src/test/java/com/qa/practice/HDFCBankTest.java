cd1834b25792f84df8e9e29a737ba63f1ddff2d3
bharatbarhate <bharat_automation@yahoo.com>
2019-10-06 02:38:27 -0700

    addded comments

--- DemoGit/src/test/java/com/qa/practice/HDFCBankTest.java
+++ DemoGit/src/test/java/com/qa/practice/HDFCBankTest.java
@@ -30,139 +30,127 @@ public class HDFCBankTest {
 	String baseUrl;
 
 	@BeforeMethod
-	public void setUp() throws Exception 
-	{
-		System.setProperty("webdriver.chrome.driver","D:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
-		baseUrl="https://www.hdfcbank.com/";
+		baseUrl = "https://www.hdfcbank.com/";
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();	
+		driver.manage().window().maximize();
 		driver.get(baseUrl);
 	}
+
 	@Test
 	public void test1() throws InterruptedException {
 		System.out.println("@Test1");
 		WebDriverWait wait = new WebDriverWait(driver, 10);
-		//		WebElement popBanner = driver.findElement(By.xpath("//img[contains(@class,'popupbanner at-element-click-tracking')]"));
-		
-		
+		// WebElement popBanner =
+		// driver.findElement(By.xpath("//img[contains(@class,'popupbanner
+		// at-element-click-tracking')]"));
+
 		try {
 			WebElement popBanner = driver.findElement(By.xpath("//img[contains(@class,'popupbanner')]"));
 			wait.until(ExpectedConditions.visibilityOf(popBanner)).isDisplayed();
-			//		driver.findElement(By.xpath("//img[@class='popupbanner at-element-click-tracking']/../following-sibling::img[@class='popupCloseButton']")).click();
+			// driver.findElement(By.xpath("//img[@class='popupbanner
+			// at-element-click-tracking']/../following-sibling::img[@class='popupCloseButton']")).click();
 			driver.findElement(By.xpath("//img[@class='popupCloseButton']")).click();
 			System.out.println("Popup 1 closed");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-		//	e.printStackTrace();
-		//	System.out.println(e);
-		//	e.getCause();
+			// e.printStackTrace();
+			// System.out.println(e);
+			// e.getCause();
 		}
 
-		String firstwindowhandle= driver.getWindowHandle();
-		System.out.println("Initial  Window Title :: "+driver.getTitle());
+		String firstwindowhandle = driver.getWindowHandle();
+		System.out.println("Initial  Window Title :: " + driver.getTitle());
 		System.out.println(firstwindowhandle);
 		driver.findElement(By.id("loginsubmit")).click();
-		Set allwindows= driver.getWindowHandles();
+		Set allwindows = driver.getWindowHandles();
 
-		if (allwindows.size()>1) {
+		if (allwindows.size() > 1) {
 			Iterator<String> itr = allwindows.iterator();
-			while(itr.hasNext())
-			{
+			while (itr.hasNext()) {
 				String firstwindow = itr.next();
 				String secondwindow = itr.next();
-				System.out.println(firstwindow+":: 2nd "+secondwindow);
-				if(firstwindow!=secondwindow) {
+				System.out.println(firstwindow + ":: 2nd " + secondwindow);
+				if (firstwindow != secondwindow) {
 					driver.switchTo().window(secondwindow);
-					System.out.println("Second Window Title :: "+driver.getTitle());
-					WebElement contNetBanking = driver.findElement(By.xpath("(//a[text()='Continue to NetBanking'])[2]"));
+					System.out.println("Second Window Title :: " + driver.getTitle());
+					WebElement contNetBanking = driver
+							.findElement(By.xpath("(//a[text()='Continue to NetBanking'])[2]"));
 					wait.until(ExpectedConditions.visibilityOf(contNetBanking));
 					contNetBanking.click();
 				}
 				allwindows.remove(secondwindow);
-				//	driver.switchTo().window(firstwindow).close();
+				// driver.switchTo().window(firstwindow).close();
 			}
 		}
 
-		for(String allinsidewindowshandles : driver.getWindowHandles()) {
+		for (String allinsidewindowshandles : driver.getWindowHandles()) {
 
-			System.out.println("All New ewindowshandles :: "+allinsidewindowshandles.toString());
-			if (driver.switchTo().window(allinsidewindowshandles).getTitle().equals("Welcome to HDFC Bank NetBanking"))
-			{
+			System.out.println("All New ewindowshandles :: " + allinsidewindowshandles.toString());
+			if (driver.switchTo().window(allinsidewindowshandles).getTitle()
+					.equals("Welcome to HDFC Bank NetBanking")) {
 				driver.switchTo().window(allinsidewindowshandles);
-				//Welcome to HDFC Bank NetBanking
-				System.out.println("Latest Title :: "+driver.getTitle());
+				// Welcome to HDFC Bank NetBanking
+				System.out.println("Latest Title :: " + driver.getTitle());
 				driver.switchTo().frame("login_page");
-				//								System.out.println(driver.findElement(By.xpath("//td[contains(text(),'User ID / Customer ID')]")).getText());
+				// System.out.println(driver.findElement(By.xpath("//td[contains(text(),'User ID
+				// / Customer ID')]")).getText());
 				driver.findElement(By.xpath("//input[@name='fldLoginUserId']")).sendKeys("12345678");
 				driver.findElement(By.xpath("//img[@alt='continue']")).click();
 				Thread.sleep(5000);
 				driver.findElement(By.xpath("//input[@name='fldPassword']")).sendKeys("Test@12345");
-				//	driver.findElement(By.xpath("//img[@alt='Login']")).click();
 
-				//								WebElement lblwrongpass = driver.findElement(By.xpath("//strong[contains(text(),'The Customer ID/IPIN (password) you have entered is invalid, please try again')]"));
-				//								wait.until(ExpectedConditions.visibilityOf(lblwrongpass)).isDisplayed();
-
-				if (driver.findElement(By.xpath("//img[@alt='Login']")).isDisplayed())
-				{
+				if (driver.findElement(By.xpath("//img[@alt='Login']")).isDisplayed()) {
 					System.out.println("Test Case Pass");
-				}
-				else
-				{
+				} else {
 					System.out.println("Test Case Failed");
 				}
 				driver.switchTo().defaultContent();
-				System.out.println("Title post Default content :: "+driver.getTitle());
-			}	
+				System.out.println("Title post Default content :: " + driver.getTitle());
+			}
 			driver.switchTo().window(firstwindowhandle);
-			System.out.println("Title post swicthing on to First Handle :: "+driver.getTitle());
+			System.out.println("Title post swicthing on to First Handle :: " + driver.getTitle());
 
 		}
 	}
 
-
-	public void addName() throws InterruptedException
-	{
+	public void addName() throws InterruptedException {
 		driver.findElement(By.name("firstname")).sendKeys("Bharat");
 		driver.findElement(By.name("lastname")).sendKeys("Barhate");
 
-		if(!driver.findElement(By.xpath("//input[@value='Male' and @type='radio']")).isSelected()) {
+		if (!driver.findElement(By.xpath("//input[@value='Male' and @type='radio']")).isSelected()) {
 			driver.findElement(By.xpath("//input[@value='Male' and @type='radio']")).click();
-		}
-		else
-		{
+		} else {
 			System.out.println("Male is already selected");
 		}
 
-		if(!driver.findElement(By.xpath("//input[@value='6' and @type='radio']")).isSelected()) {
+		if (!driver.findElement(By.xpath("//input[@value='6' and @type='radio']")).isSelected()) {
 			driver.findElement(By.xpath("//input[@value='6' and @type='radio']")).click();
-		}
-		else
-		{
+		} else {
 			System.out.println("Years of Exp is already selected");
 		}
 
-		if(driver.findElement(By.id("datepicker")).isDisplayed()) {
+		if (driver.findElement(By.id("datepicker")).isDisplayed()) {
 
 			DateTimeFormatter dtformat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 			LocalDateTime localtDate = LocalDateTime.now();
 
-			//			SimpleDateFormat formDate = new SimpleDateFormat("MM/dd/yyyy");
+			// SimpleDateFormat formDate = new SimpleDateFormat("MM/dd/yyyy");
 			// String strDate = formDate.format(System.currentTimeMillis()); // option 1
-			//	String tDate = formDate.format(new Date()); // option 2
+			// String tDate = formDate.format(new Date()); // option 2
 
 			String tDate = dtformat.format(localtDate);
 			System.out.println(tDate);
 			driver.findElement(By.id("datepicker")).sendKeys(tDate);
 		}
 
-		if(!driver.findElement(By.xpath("//input[@type='checkbox' and @value='Selenium Webdriver']")).isSelected()) {
+		if (!driver.findElement(By.xpath("//input[@type='checkbox' and @value='Selenium Webdriver']")).isSelected()) {
 			driver.findElement(By.xpath("//input[@type='checkbox' and @value='Selenium Webdriver']")).click();
-		}
-		else
-		{
+		} else {
 			System.out.println("Selenium Webdriver is already selected");
 		}
 
@@ -172,7 +160,7 @@ public class HDFCBankTest {
 
 		WebElement cmbselCommands = driver.findElement(By.id("selenium_commands"));
 		Select selectselCommands = new Select(cmbselCommands);
-		//	selectselCommands.selectByVisibleText("WebElement Commands");
+		// selectselCommands.selectByVisibleText("WebElement Commands");
 		List<WebElement> lstselCommands = selectselCommands.getOptions();
 		for (int i = 0; i < lstselCommands.size(); i++) {
 			System.out.println(lstselCommands.get(i).getText());
@@ -184,8 +172,7 @@ public class HDFCBankTest {
 	}
 
 	@AfterMethod
-	public void tearDown()
-	{
+	public void tearDown() {
 		driver.quit();
 	}
 }
